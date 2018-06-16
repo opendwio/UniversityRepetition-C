e224a986a75efb71918ce3b9a148415d6cd900fd
opendwio <fdx3@gmx.de>
2018-06-16 11:23:42 +0200

    added 'hello world' files

---
+++ hello world.c
@@ -0,0 +1,11 @@
+/********************************
+*hello world.c					*
+*the most basic C-Application	*
+*Author: opendwio				*
+*Date: 16.06.18					*
+*********************************/
+#include <stdio.h>
+int main() {
+	printf("Hello World!\n"); // \n = escape sequence
+return 0;
+}
