9189f7d4478ea06cccf3e004821a4195683c3c5d
opendwio <fdx3@gmx.de>
2018-06-17 22:29:34 +0200

    added 'callbyrefAddition' files

---
+++ exercises/callbyrefAddition.c
@@ -0,0 +1,21 @@
+/************************************************************
+*callbyrefAddition.c										*
+*a simple program to add 2 numbers using Call by Reference	*
+*Author: opendwio											*
+*Date: 17.06.18												*
+*************************************************************/
+#include <stdio.h>
+int add(int a, int b, int *c){
+	*c =a +b;
+	return *c ;
+}
+int main() {
+	int x, y;
+	int *c;
+	printf("Input 2 numbers\n");
+	scanf("%i%i", &x,&y);
+	add(x,y,c);
+	printf("%i + %i = %i\n", x,y,*c);
+	
+	return 0;
+}
